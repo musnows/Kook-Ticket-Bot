4641aaea39cdd4a516b147824716dd901029066e
Ewait <s2522446388@163.com>
2022-12-12 20:44:36 +0800

    feat(main): add help command

--- code/main.py
+++ code/main.py
@@ -49,7 +49,10 @@ def help_text():
     text = "ticket-bot的命令操作\n"
     text+=f"`/ticket` 在本频道发送一条消息，作为ticket的开启按钮\n"
     text+=f"`/tkcm 工单id 备注` 对某一条已经关闭的工单进行备注\n"
-    text+=f"以上命令都需要管理员才能操作"
+    text+=f"以上命令都需要管理员才能操作\n"
+    text+=f"`/gaming 游戏选项` 让机器人开始打游戏(代码中指定了几个游戏)\n"
+    text+=f"`/singing 歌名 歌手` 让机器人开始听歌\n"
+    text+=f"`/sleeping 1(2)` 让机器人停止打游戏1 or 听歌2\n"
     return text
 
 @bot.command(name='TKhelp')

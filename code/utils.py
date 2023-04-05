96d6f49ab1eae9c0b41113a48f2c0e2eeca8d426
musnow <ezplayingd@126.com>
2023-04-05 14:15:15 +0800

    refactor(utils): using datetime timezone to replace zoneinfo

--- code/utils.py
+++ code/utils.py
@@ -4,13 +4,13 @@ import sys
 import traceback
 from khl import Message,Event
 
-from datetime import datetime
-from zoneinfo import ZoneInfo
+from datetime import datetime,timezone,timedelta
 
 def GetTime():
-    """获取当前时间，格式为 `23-01-01 00:00:00`"""
-    a = datetime.now(ZoneInfo('Asia/Shanghai')) # 返回北京时间
-    return a.strftime('%y-%m-%d %H:%M:%S')
+    """获取当前时间，格式为 `23-01-01 00:00:00`"""    
+    utc_dt = datetime.now(timezone.utc) # 获取当前时间
+    bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8))) # 转换为北京时间
+    return bj_dt.strftime('%y-%m-%d %H:%M:%S')
     # use time.loacltime if you aren't using BeiJing Time
     # return time.strftime("%y-%m-%d %H:%M:%S", time.localtime())
 
